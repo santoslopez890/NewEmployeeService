a1fa28843b97151ceda299efe8bf86d2ecf81eb1
Santos Herrera <santoslopez890@gmail.com>
2024-12-21 23:14:05 -0500

    Created Model Mapper

---
+++ src/main/java/com/employeeapp/employee/service/Config/EmployeeAppConfig.java
@@ -0,0 +1,13 @@
+package com.employeeapp.employee.service.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeAppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
--- src/main/java/com/employeeapp/employee/service/Service/EmployeeService.java
+++ src/main/java/com/employeeapp/employee/service/Service/EmployeeService.java
@@ -1,22 +1,29 @@
 package com.employeeapp.employee.service.Service;
 
+import com.employeeapp.employee.service.entity.Employee;
 import com.employeeapp.employee.service.response.EmployeeResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Autowired
     private com.employeeapp.employee.service.Repo.EmployeeRepo EmployeeRepo;
 
     //employee object-employee respince
     public EmployeeResponse getEmployeeByID(int id){
-        EmployeeResponse employeeResponse=new EmployeeResponse();
-        employeeResponse.setId(EmployeeRepo.findById(id).get().getId());
-        employeeResponse.setName(EmployeeRepo.findById(id).get().getName());
-        employeeResponse.setEmail(EmployeeRepo.findById(id).get().getEmail());
-        employeeResponse.setBloodgroup(EmployeeRepo.findById(id).get().getBloodgroup());
+        Employee employee=EmployeeRepo.findById(id).get();
+        EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
+//        EmployeeResponse employeeResponse=new EmployeeResponse();
+//        employeeResponse.setId(EmployeeRepo.findById(id).get().getId());
+//        employeeResponse.setName(EmployeeRepo.findById(id).get().getName());
+//        employeeResponse.setEmail(EmployeeRepo.findById(id).get().getEmail());
+//        employeeResponse.setBloodgroup(EmployeeRepo.findById(id).get().getBloodgroup());
+
         return employeeResponse;
     }
 }
